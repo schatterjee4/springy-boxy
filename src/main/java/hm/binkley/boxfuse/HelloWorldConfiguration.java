a4c224ef08f80328e1060c5db9419e0e7fdf2893
Brian Oxley <boxley@thoughtworks.com>
2015-11-17 16:27:46 -0500

    Re-enable bits not in actual conflict with Spring.

--- src/main/java/hm/binkley/boxfuse/HelloWorldConfiguration.java
+++ src/main/java/hm/binkley/boxfuse/HelloWorldConfiguration.java
@@ -3,6 +3,10 @@ package hm.binkley.boxfuse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley (binkley)</a>
+ * @todo Do something useful
+ */
 @SpringBootApplication
 public class HelloWorldConfiguration {
     public static void main(final String... args) {
