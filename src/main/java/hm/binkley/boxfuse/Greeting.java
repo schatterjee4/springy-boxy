294c086f0e177821d1e40088e5e61ed0ead06f12
Brian Oxley <boxley@thoughtworks.com>
2015-11-17 16:37:38 -0500

    Add Travis CI with deployment.

--- src/main/java/hm/binkley/boxfuse/Greeting.java
+++ src/main/java/hm/binkley/boxfuse/Greeting.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.annotation.Nonnull;
+
 @EqualsAndHashCode
 @Getter
 @RequiredArgsConstructor
 @ToString
 public class Greeting {
     private final long id;
+    @Nonnull
     private final String content;
 }
