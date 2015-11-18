052ef2004f59bb9edebf3149b81a84e3c5f56423
Brian Oxley <boxley@thoughtworks.com>
2015-11-17 20:07:58 -0500

    Basic unit test.

--- src/main/java/hm/binkley/boxfuse/HelloWorldController.java
+++ src/main/java/hm/binkley/boxfuse/HelloWorldController.java
@@ -1,25 +1,27 @@
 package hm.binkley.boxfuse;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import static hm.binkley.boxfuse.HelloWorldController.PATH;
 import static java.lang.String.format;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@Controller
-@RequestMapping("/hello-world")
+@RestController
+@RequestMapping(PATH)
 public class HelloWorldController {
+    public static final String PATH = "/hello-world";
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public Greeting sayHello(@RequestParam(value = "name", required = false,
-            defaultValue = "Stranger") final String name) {
+    @RequestMapping(value = "/{name}", method = GET)
+    public Greeting sayHello(@PathVariable("name") final String name) {
         return new Greeting(counter.incrementAndGet(),
                 format(template, name));
     }
---
+++ src/test/java/hm/binkley/boxfuse/HelloWorldControllerTest.java
@@ -0,0 +1,39 @@
+package hm.binkley.boxfuse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class HelloWorldControllerTest {
+    private MockMvc mvc;
+
+    @Before
+    public void setUp()
+            throws Exception {
+        mvc = standaloneSetup(new HelloWorldController()).build();
+    }
+
+    @Test
+    public void getHello()
+            throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello-world/Brian").
+                accept(APPLICATION_JSON)).
+                andExpect(status().isOk()).
+                andExpect(content()
+                        .json("{\"id\":1,\"content\":\"Hello, Brian!\"}"));
+    }
+}
