e9a6e99fc8fd4deeaf3852efd72135ae727e5ec9
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2015-12-26 14:59:48 -0600

    Cleaner.

--- src/test/java/hm/binkley/boxfuse/HelloWorldControllerIT.java
+++ src/test/java/hm/binkley/boxfuse/HelloWorldControllerIT.java
@@ -6,10 +6,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import static com.jayway.restassured.RestAssured.when;
 import static hm.binkley.boxfuse.HelloWorldController.PATH;
@@ -19,8 +18,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Main.class)
-@IntegrationTest({"server.port:0", "management.port:0"})
-@WebAppConfiguration
+@WebIntegrationTest({"server.port:0", "management.port:0"})
 public class HelloWorldControllerIT {
     @Value("${local.server.port}")
     private int port;
--- src/test/java/hm/binkley/boxfuse/HelloWorldControllerTest.java
+++ src/test/java/hm/binkley/boxfuse/HelloWorldControllerTest.java
@@ -6,20 +6,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static hm.binkley.boxfuse.HelloWorldController.PATH;
 import static java.lang.String.format;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MockServletContext.class)
-@WebAppConfiguration
 public class HelloWorldControllerTest {
     private MockMvc mvc;
 
@@ -34,7 +32,7 @@ public class HelloWorldControllerTest {
             throws Exception {
         mvc.perform(
                 MockMvcRequestBuilders.get(format("%s/%s", PATH, "Brian")).
-                        accept(APPLICATION_JSON)).
+                        accept(APPLICATION_JSON_UTF8)).
                 andExpect(status().isOk()).
                 andExpect(content().
                         json("{\"id\":1,\"content\":\"Hello, Brian!\"}"));
