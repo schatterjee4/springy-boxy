2abe3a5760df741db5322c8b31bde3901d6a5071
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2016-01-10 09:59:52 -0600

    Static instance.

--- src/main/java/hm/binkley/boxfuse/EnabledWebMvcConfigurationSupport.java
+++ src/main/java/hm/binkley/boxfuse/EnabledWebMvcConfigurationSupport.java
@@ -2,13 +2,15 @@ package hm.binkley.boxfuse;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
 public class EnabledWebMvcConfigurationSupport
         extends WebMvcConfigurationSupport {
+    private static final EnabledRequestMappingHandlerMapping enabledMapping
+            = new EnabledRequestMappingHandlerMapping();
+
     @Override
-    protected RequestMappingHandlerMapping createRequestMappingHandlerMapping() {
-        return new EnabledRequestMappingHandlerMapping();
+    protected EnabledRequestMappingHandlerMapping createRequestMappingHandlerMapping() {
+        return enabledMapping;
     }
 }
